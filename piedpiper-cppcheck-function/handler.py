3c27a808612f66db2bbbe5b37eb3991aaefb2c3f
JCantu248 <jcantu248@gmail.com>
2019-03-28 13:40:17 -0500

    Add options passing to cppcheck command

--- piedpiper-cppcheck-function/handler.py
+++ piedpiper-cppcheck-function/handler.py
@@ -2,6 +2,8 @@ import os
 import sh
 from io import StringIO
 from .util import build_temp_zipfiles, build_directories, unzip_files
+import yaml 
+from pathlib import Path
 
 
 def handle(request):
@@ -9,7 +11,7 @@ def handle(request):
     Args:
         req (str): request body
     """
-
+    
     zip_files = build_temp_zipfiles(request)
     temp_directories = build_directories(request)
     cppcheck_reports = []
@@ -17,7 +19,15 @@ def handle(request):
     for zip_file, temp_directory in zip(zip_files, temp_directories):
         unzip_files(zip_file, temp_directory.name)
         os.chdir(temp_directory.name)
-        report = run_cppcheck('.')
+
+        options = Path("options.yml")
+        if(options.is_file() == False or options.exists() == False):
+           #options.yml not found, resorting to defaults
+            report = run_cppcheck('.')
+        else:
+            with open('options.yaml') as f:
+                dataMap = yaml.load(f)
+                report = run_cppcheck_w_opt('.', dataMap['options'])
         cppcheck_reports.append(report)
 
     return '\n'.join(cppcheck_reports)
@@ -25,7 +35,14 @@ def handle(request):
 def run_cppcheck(directory):
     buf = StringIO()
     try:
-        sh.cppcheck("--verbose", directory, _err=buf)
+       sh.cppcheck(directory, _out=buf, _err_to_out=True)
     except sh.ErrorReturnCode_1 as e:
         pass
+    return buf.getvalue()
+
+def run_cppcheck_w_opt(directory, cmd_options)
+    buf = StringIO()
+    try:
+        sh.cppcheck(options, directory, _out=buf, _err_to_out=True)
+
     return buf.getvalue()

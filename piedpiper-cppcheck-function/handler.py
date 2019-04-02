cc05c5bf92324ada32b74eecf012c9ad2a3b97fd
JCantu248 <jcantu248@gmail.com>
2019-04-02 14:54:23 -0500

    Changes 4/2 difficulty debugging scripts

--- piedpiper-cppcheck-function/handler.py
+++ piedpiper-cppcheck-function/handler.py
@@ -11,7 +11,6 @@ def handle(request):
     Args:
         req (str): request body
     """
-    
     zip_files = build_temp_zipfiles(request)
     temp_directories = build_directories(request)
     cppcheck_reports = []
@@ -19,31 +18,36 @@ def handle(request):
     for zip_file, temp_directory in zip(zip_files, temp_directories):
         unzip_files(zip_file, temp_directory.name)
         os.chdir(temp_directory.name)
-
-        options = Path("options.yml")
+        print (temp_directory.name)
+        options = Path("run_vars.yml")
         if(options.is_file() == False or options.exists() == False):
            #options.yml not found, resorting to defaults
             report = run_cppcheck('.')
         else:
-            with open('options.yaml') as f:
+            with open('run_vars.yml') as f:
                 dataMap = yaml.load(f)
-                report = run_cppcheck_w_opt('.', dataMap['options'])
+                report = run_cppcheck('.', dataMap['options'])
         cppcheck_reports.append(report)
 
     return '\n'.join(cppcheck_reports)
 
-def run_cppcheck(directory):
+"""def run_cppcheck(directory):
     buf = StringIO()
     try:
        sh.cppcheck(directory, _out=buf, _err_to_out=True)
     except sh.ErrorReturnCode_1 as e:
         pass
     return buf.getvalue()
-
-def run_cppcheck_w_opt(directory, cmd_options):
+"""
+def run_cppcheck(directory, cmd_options = None):
     buf = StringIO()
+ 
     try:
-        sh.cppcheck(cmd_options, directory, _out=buf, _err_to_out=True)
+        if (cmd_options == None):
+            sh.cppcheck(directory, _out=buf, _err_to_out=True)
+        else:
+            sh.cppcheck(cmd_options, directory, _out=buf, _err_to_out=True)
     except sh.ErrorReturnCode_1 as e:
+        print (e)
         pass
     return buf.getvalue()
